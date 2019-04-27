b7e0421d759ea6e02bfc465e495be095ef6b42b5
Amy Holt <amy.elizabeth.holt@gmail.com>
2019-04-27 15:28:15 -0600

    Complete Iteration 1

--- emojional-ios-key/ViewController.swift
+++ emojional-ios-key/ViewController.swift
@@ -12,9 +12,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "title", message: "message", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
 
+    }
+    
 }
 
